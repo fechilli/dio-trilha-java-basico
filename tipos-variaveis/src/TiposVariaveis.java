708fd00a06b0c6c215f9e6debfbcd32fbedde569
fechilli <luizfdas@outlook.com>
2024-04-30 08:40:05 -0300

    aula de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto*6;
+        short numeroCurto2 = (short) numeroNormal;
+        final float PI = 3.14f;
+        
+       
+
     }
 }
